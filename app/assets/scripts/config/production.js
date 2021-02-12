373407082d53ce473ba69061c82c473d90a0b923
Olaf Veerman <olaf.veerman@gmail.com>
2021-02-12 15:29:56 +0000

    Scaffold app using DS project seed

---
+++ app/assets/scripts/config/production.js
@@ -0,0 +1,8 @@
+// module exports is required to be able to load from gulpfile.
+module.exports = {
+  default: {
+    environment: 'production',
+    appTitle: 'Africa Electrification Platform',
+    appDescription: 'Africa Electrification Platform - geospatial analysis for an energy secure future.'
+  }
+};
