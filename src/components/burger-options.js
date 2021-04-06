609e7dc43a4b9f4dda2c3c9e9824879fd99dec8f
Daniel da Silva <danielfdsilva@gmail.com>
2021-04-06 11:16:21 +0100

    Fix active state on responsive dropdown menu

--- src/components/burger-options.js
+++ src/components/burger-options.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import T from 'prop-types';
+import styled from 'styled-components';
 import { Button } from '@devseed-ui/button';
 import Dropdown, {
   DropTitle,
   DropMenu,
   DropMenuItem
 } from '@devseed-ui/dropdown';
+import { themeVal, glsp } from '@devseed-ui/theme-provider';
+import collecticon from '@devseed-ui/collecticons';
 
 import { Link } from '../styles/clean/link';
+
+// TODO: Remove once ui library is updated.
+const DropMenuItemLink = styled(DropMenuItem)`
+  &.active {
+    color: ${themeVal('color.primary')};
+
+    &::after {
+      ${collecticon('tick--small')}
+      position: absolute;
+      z-index: 1;
+      top: ${glsp(1 / 4)};
+      right: ${glsp(1 / 2)};
+      font-size: 1rem;
+      line-height: 1.5rem;
+      width: 1.5rem;
+      text-align: center;
+    }
+  }
+`;
 
 function BurgerOptions(props) {
   const { items } = props;
@@ -29,10 +51,10 @@ function BurgerOptions(props) {
       )}
     >
       <DropTitle>Browse</DropTitle>
-      <DropMenu role='menu'>
+      <DropMenu role='menu' selectable>
         {items.map((l) => (
           <li key={l.url}>
-            <DropMenuItem
+            <DropMenuItemLink
               as={Link}
               activeClassName='active'
               partiallyActive={l.partiallyActive}
@@ -41,7 +63,7 @@ function BurgerOptions(props) {
               data-dropdown='click.close'
             >
               {l.label}
-            </DropMenuItem>
+            </DropMenuItemLink>
           </li>
         ))}
       </DropMenu>
