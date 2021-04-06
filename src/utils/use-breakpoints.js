bbf0d3ca613a71d7d5c959d533eb306b8e9783a5
Daniel da Silva <danielfdsilva@gmail.com>
2021-04-06 10:07:57 +0100

    Fix useBreakpoints SSR

--- src/utils/use-breakpoints.js
+++ src/utils/use-breakpoints.js
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from 'react';
 import { ThemeContext } from 'styled-components';
 
 const calculateBreakpoints = (ranges) => {
-  const w = window.innerWidth;
+  const w = typeof window === 'undefined' ? 0 : window.innerWidth;
   const mediaKeys = Object.keys(ranges);
   return mediaKeys.reduce((acc, k) => {
     const [lower, upper] = ranges[k];
