f14e1404ccb43eda1dfd315102fbdfd03e5e3120
Ricardo Mestre <ricardo@duplos.org>
2021-04-11 21:26:47 +0100

    Improve details list display on small screens

--- src/styles/typography/details-list.js
+++ src/styles/typography/details-list.js
@@ -6,20 +6,32 @@ import { headingAlt } from '@devseed-ui/typography';
 const DetailsList = styled.dl`
   display: grid;
   grid-template-columns: 1fr;
-  grid-gap: ${glsp(0.25, 1)};
+  grid-gap: ${glsp(0, 1)};
 
   ${media.smallUp`
     grid-template-columns: minmax(min-content, max-content) 1fr;
-    grid-gap: ${glsp(0.5, 2)};
+    grid-gap: ${glsp(0.25, 2)};
   `}
 
   dt {
     ${headingAlt()}
-    line-height: inherit;
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+    padding: ${glsp(0.125, 0)};
   }
 
   dd {
     font-weight: ${themeVal('type.base.bold')};
+    font-size: 1rem;
+    line-height: 1.5rem;
+
+    &:not(:last-child) {
+      margin-bottom: ${glsp(0.5)};
+
+      ${media.smallUp`
+        margin-bottom: 0;
+      `}
+    }
   }
 `;
 
